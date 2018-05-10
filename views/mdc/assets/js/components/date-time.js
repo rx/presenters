2ed8c8b80b09c7ae57e4ee49ce6ff825749f2aa8
Russell Edens <rx@voomify.com>
2018-05-10 04:48:50 -0400

    WIP flatpickr date time

--- views/mdc/assets/js/components/date-time.js
+++ views/mdc/assets/js/components/date-time.js
@@ -1,5 +1,12 @@
+import flatpickr from "flatpickr";
+
 export function initDateTime() {
     console.log('\tDateTime');
+    var components = document.querySelectorAll('.v-datetime');
+        for (var i = 0; i < components.length; i++) {
+            var component = components[i];
+            flatpickr(component, {});
+        }
 }
 
 
