33ef0b30e3b8101af6fd73b165603d8649d78ca8
Kurt Grafius <kurtg@voomify.com>
2018-07-11 17:01:06 -0700

    Converted text_area to MDC

--- lib/voom/presenters/dsl/components/text_area.rb
+++ lib/voom/presenters/dsl/components/text_area.rb
@@ -5,11 +5,12 @@ module Voom
     module DSL
       module Components
         class TextArea < TextField
-          attr_reader :rows
+          attr_reader :rows, :cols
           
           def initialize(**attribs_, &block)
             super(type: :text_area, **attribs_, &block)
             @rows = attribs.delete(:rows) || 3
+            @cols = attribs.delete(:cols) || 40 unless full_width
             expand!
           end
         end
