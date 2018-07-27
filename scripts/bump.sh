7cb7677a15a43827af75fe29254d3be7d7424b16
Russell Edens <rx@voomify.com>
2018-07-27 12:05:30 -0400

    Added gem-release to dev dependencies.

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+# Docs: https://github.com/svenfuchs/gem-release#gem-release
 gem bump
 
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Docs: https://github.com/svenfuchs/gem-release#gem-release
 gem tag
 gem release
 
