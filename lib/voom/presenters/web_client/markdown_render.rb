b27adba5b6d6e4be4595f36a7c38ee9794d8dd7b
Russell Edens <rx@voomify.com>
2018-08-27 12:54:21 -0400

    Added postprocessor to strip trailing <br/> from markdown. It was messing up the list line layout.

--- lib/voom/presenters/web_client/markdown_render.rb
+++ lib/voom/presenters/web_client/markdown_render.rb
@@ -1,16 +1,27 @@
 class CustomRender < Redcarpet::Render::Safe
   include Redcarpet::Render::SmartyPants
-
+  BR = '<br/>'
+  
   def initialize(extensions = {})
     super(extensions)
   end
   
   def paragraph(text)
-      text + '<br/>'
+      text + BR
     end
 
   def postprocess(full_document)
-    full_document.
-        gsub(/{c:([#\w]\w+)}([^{]+){\/c}/) {|m| "<span style=\"color:#{$1};\">#{$2}</span>"} # color
+    strip_trailing_br(colorize(full_document))
+  end
+
+  private
+
+  def colorize(doc)
+    doc.gsub(/{c:([#\w]\w+)}([^{]+){\/c}/) {|m| "<span style=\"color:#{$1};\">#{$2}</span>"}
+  end
+
+  def strip_trailing_br(doc)
+    return doc unless doc.reverse[0...BR.length]==BR.reverse
+    doc[0...doc.length-BR.length]
   end
 end
