666a157ef706c9be39a5ba95e994e07f6cea2d85
Russell Edens <rx@voomify.com>
2018-08-29 09:00:43 -0400

    Fixed fat fingered bug.

--- lib/voom/presenters/dsl/namespace.rb
+++ lib/voom/presenters/dsl/namespace.rb
@@ -4,7 +4,7 @@ module Voom
       module Namespace
         def _expand_namespace_(presenter, namespace)
           presenter = presenter.to_s
-          return namespace  if presenter.split(':').size > 1
+          return presenter  if presenter.split(':').size > 1
           namespace.any? ? namespace.join(':')+':'+presenter : presenter
         end
       end
