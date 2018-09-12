e53dbbf9c826b989ba7716814e0c289533846219
Russell Edens <rx@voomify.com>
2018-09-12 14:34:04 -0400

    Added timezone and auto formatting of columns that end with _at and _on.

--- lib/voom/presenters/helpers/rails/model_table.rb
+++ lib/voom/presenters/helpers/rails/model_table.rb
@@ -7,6 +7,7 @@ module Voom
           def table_for(query_,
                         empty_icon: 'far fa-folder-open fa-rotate-180',
                         except: [:id, :created_at, :updated_at],
+                        timezone: nil, # The timezone to covnert _at and _on values
                         selectable: false, &block)
             row1 = query_.first
             if row1
@@ -16,25 +17,32 @@ module Voom
 
                 query_.each do |item_|
                   row do
-                    _table_row_(__columns__, block, except, item_)
+                    _table_row_(__columns__, block, except, item_, timezone: timezone)
                   end
                 end
               end
             else
-              body "You have no #{query_.arel_table.name.pluralize}", level: 2
-              icon empty_icon, size: '10rem' if empty_icon
+              icon empty_icon, size: '6rem' if empty_icon
+              body "You have no #{query_.arel_table.name.pluralize.humanize}"
             end
           end
 
 
           private
-          def _table_row_(__columns__, block, except, item_)
+          def _table_row_(__columns__, block, except, item_, timezone: nil)
             __columns__.each do |col|
               next if except.include?(col.name.to_sym)
-              column item_.send(col.name.to_sym) do
+                value = format_value(col.name,item_.send(col.name.to_sym), timezone: timezone)
+              column value do
                 self.instance_exec(item_, col.name.to_sym, &block) if block
               end
             end
+          end
+
+          def format_value(col_name, value, timezone:)
+            return format_time_long(value, timezone: timezone) if timezone &&
+                col_name.ends_with?('_at') || col_name.ends_with?('_on')
+            value
           end
 
           def _table_header_(__columns__, except)
