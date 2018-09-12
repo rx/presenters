afd4d5fbe21a12343540f78a13eccb297b1c9471
Russell Edens <rx@voomify.com>
2018-09-12 14:35:24 -0400

    Added delay to focus event, so we can receive a click event on a floating content block before its invisible and can't receive events.

--- views/mdc/assets/js/components/events/toggle_visibility.js
+++ views/mdc/assets/js/components/events/toggle_visibility.js
@@ -8,18 +8,22 @@ export class VToggleVisibility  {
     call(results) {
         let targetId = this.targetId;
         let action = this.params.action;
+        let delayAmt = this.event instanceof FocusEvent ? 500 : 0;
+        let elem = document.getElementById(targetId);
         let promiseObj = new Promise(function (resolve) {
-            console.log("Toggling visibility on: " + targetId);
-            let elem = document.getElementById(targetId);
-            if (action === 'show') {
-                elem.classList.remove("v-hidden");
-            } else if (action === 'hide') {
-                elem.classList.add("v-hidden");
-            } else {
-                elem.classList.toggle("v-hidden");
-            }
-            results.push({action:'toggle_visibility', statusCode: 200});
-            resolve(results);
+            clearTimeout(elem.vTimeout);
+            elem.vTimeout = setTimeout(function(){
+                console.log("Toggling visibility on: " + targetId);
+                if (action === 'show') {
+                    elem.classList.remove("v-hidden");
+                } else if (action === 'hide') {
+                    elem.classList.add("v-hidden");
+                } else {
+                    elem.classList.toggle("v-hidden");
+                }
+                results.push({action:'toggle_visibility', statusCode: 200});
+                resolve(results);
+            }, delayAmt);
         });
         return promiseObj;
     }
