a0db7c8b6f06d6ca8e02acdc3c1383d38af963f8
Nick Miller <nick@nmiller.info>
2018-10-30 15:51:13 -0400

    Add compatibility class to root element

--- views/mdc/assets/js/utils/compatibility.js
+++ views/mdc/assets/js/utils/compatibility.js
@@ -7,15 +7,15 @@ const name = browser ? browser.name : 'unknown';
 const isCompatible = browser && incompatibleList.indexOf(name) < 0;
 
 document.addEventListener('DOMContentLoaded', () => {
-    if (isCompatible) {
-        document.documentElement.classList.remove('incompatible-browser');
-    } else {
+    if (!isCompatible) {
         const errorMessage = config.get(
             'compatibility.errorMessage',
             'Unsupported browser!',
         );
         document.body.innerHTML = `<p>${errorMessage}</p>`;
     }
+
+    document.documentElement.classList.remove('incompatible-browser');
 });
 
 export default isCompatible;
