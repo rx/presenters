347e057a6b4d22ef96556cb6b60ce5c70ec85315
Nick Miller <nick@nmiller.info>
2018-10-30 14:36:37 -0400

    Add babel-polyfill
    This ensures compatibility with Microsoft Edge.

--- views/mdc/assets/js/app.js
+++ views/mdc/assets/js/app.js
@@ -2,6 +2,7 @@ require('material-design-lite/material');
 require('./mdl-stepper');
 window.dialogPolyfill = require('./dialog-polyfill');
 import {initialize} from './components/initialize';
+require('babel-polyfill');
 
 document.addEventListener("DOMContentLoaded", function(event) {
     initialize();
