f3cba2d92193ff9d315045679365f825e3ba8d53
Kurt Grafius <kurtg@voomify.com>
2018-12-05 14:22:30 -0800

    Fixed height accessor and specs.

--- lib/voom/presenters/dsl/components/rich_text_area.rb
+++ lib/voom/presenters/dsl/components/rich_text_area.rb
@@ -5,25 +5,15 @@ module Voom
     module DSL
       module Components
         class RichTextArea < TextField
-          attr_reader :placeholder
+          attr_reader :placeholder, :height
 
           def initialize(**attribs_, &block)
             super(type: :rich_text_area, **attribs_, &block)
-            @rows = attribs.delete(:rows) || default(:rows)
             @placeholder = attribs.delete(:placeholder)
+            @rows = attribs.delete(:rows) || default(:rows)
+            @height = "#{@rows * 24}px"
             expand!
           end
-
-          def height(height=nil)
-            return @height if locked?
-            @height ||= "#{@rows * 24}px"
-          end
-
-          def value(value=nil)
-            return @value if locked?
-            @value = value
-          end
-
         end
       end
     end
