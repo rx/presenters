2ad71394f505bb10e694e7362461e3021af72b73
Russell Edens <rx@voomify.com>
2018-12-05 14:22:01 -0500

    Added MDC dialog.

--- views/mdc/assets/js/components/events/dialog.js
+++ views/mdc/assets/js/components/events/dialog.js
@@ -6,20 +6,18 @@ export class VDialog {
     }
 
     call(results) {
-        var dialog = document.querySelector('#' + this.dialogId);
-        if(dialog) {
-            if (!dialog.showModal) {
-                dialogPolyfill.registerDialog(dialog);
-            }
-
-            dialog.showModal();
-        }else {
-            console.error("Unable to find dialog with id: "+this.dialogId+". Usually this means you forgot to attach it to the currently rendered page.");
+        const dialog = document.querySelector('#' + this.dialogId);
+        if (dialog && dialog.vComponent) {
+            dialog.vComponent.open();
         }
-        var promiseObj = new Promise(function (resolve) {
-            results.push({action:'dialog', statusCode: 200});
+        else {
+            console.error('Unable to find dialog with id: ' +
+                this.dialogId + '. Usually this means you forgot ' +
+                'to attach it to the currently rendered page.');
+        }
+        return new Promise(function(resolve) {
+            results.push({action: 'dialog', statusCode: 200});
             resolve(results);
         });
-        return promiseObj;
     }
 }
