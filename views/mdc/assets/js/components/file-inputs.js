b5a94b4da1f0f6bf3abc778fc7713694b7c39518
Russell Edens <rx@voomify.com>
2019-02-15 13:05:17 -0500

    Refactor of file_inputs to limit mimetypes (accept) and to preview files.

--- views/mdc/assets/js/components/file-inputs.js
+++ views/mdc/assets/js/components/file-inputs.js
@@ -1,29 +1,60 @@
-import {VBaseContainer} from "./base-container";
-import {VBaseComponent, hookupComponents} from "./base-component";
-import {eventHandlerMixin} from "./mixins/event-handler";
+import {VBaseComponent, hookupComponents} from './base-component';
+import {eventHandlerMixin} from './mixins/event-handler';
 
 export function initFileInputs() {
-  console.log('\tFile Inputs');
-  hookupComponents('.v-file-input', VFileInput, null);
+    console.log('\tFile Inputs');
+    hookupComponents('.v-file-input', VFileInput, null);
 }
 
 export class VFileInput extends eventHandlerMixin(VBaseComponent) {
-  constructor(element, mdcComponent) {
-    super(element, mdcComponent);
+    constructor(element, mdcComponent) {
+        super(element, mdcComponent);
 
-    this.input = element.querySelector('input[type=file]');
-    this.label = element.querySelector('label');
+        this.input = element.querySelector('input[type=file]');
+        this.label = element.querySelector('label');
+        this.accept = element.dataset.accept;
+        this.preview = JSON.parse(element.dataset.preview);
+        ['change'].forEach((e) => {
+            element.addEventListener(e, (e) => this.handleFileSelection(e),
+                false);
+        });
+    }
 
-    ['change', 'mouseup'].forEach((e) => {
-      element.addEventListener(e, () => this.handleFileSelection());
-    });
-  }
+    previewComponent(e) {
+        if (!this.preview) return null;
+        for (const previewId of this.preview) {
+            const elem = document.getElementById(previewId);
+            if (elem && elem.vComponent && elem.vComponent.preview) {
+                const filename = e.target.files[0];
+                const fr = new FileReader();
+                fr.onload = (e) => {
+                    this.previewSelection(e, elem.vComponent);
+                };
+                if (this.accept.startsWith('text')) {
+                    fr.readAsText(filename);
+                }
+                else {
+                    fr.readAsDataURL(filename);
+                }
 
-  // From an example based on: https://www.quirksmode.org/dom/inputfile.html
-  handleFileSelection() {
-    if (!this.input.value) return;
+            }
+            else {
+                console.log(
+                    `WARNING: Unable to find previewable element with id: ${previewId}
+1) Make sure you have an element with that id on your page
+2) Make sure the Componenet or Plugin supports the preview option for the request mime type`);
+            }
+        }
+    }
 
-    var value = this.input.value.replace(/^.*[\\\/]/, '');
-    this.label.innerText = value;
-  }
+    previewSelection(e, previewComp) {
+        previewComp.preview(e.target.result, this.accepts, this.file);
+    }
+
+    // From an example based on: https://www.quirksmode.org/dom/inputfile.html
+    handleFileSelection(e) {
+        this.file = e.target.files[0];
+        this.label.innerText = this.file.name;
+        this.previewComponent(e);
+    }
 }
