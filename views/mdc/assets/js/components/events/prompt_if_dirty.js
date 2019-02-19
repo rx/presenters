99a7e840822e09354b0cc6ee7538826d73c5f962
jadefish <nick@nmiller.info>
2019-02-18 22:39:09 -0500

    Add input_tag support to prompt_if_dirty action

--- views/mdc/assets/js/components/events/prompt_if_dirty.js
+++ views/mdc/assets/js/components/events/prompt_if_dirty.js
@@ -27,32 +27,13 @@ export class VPromptIfDirty extends VBase {
         this.params = params;
         this.event = event;
         this.dialog = new VDialog(this.options, this.params, this.event);
-
-        // TODO: handle incoming input_tag
     }
 
     call(results) {
-        const parent = this.parentElement().vComponent;
-
-        if (!parent) {
-            const error = new Error(
-                'Unable to find a parent container! '
-                + 'Try using input_tag instead.'
-            );
-
-            results.push({
-                action: 'prompt_if_dirty',
-                contentType: 'v/errors',
-                content: {exception: error.message},
-            });
-
-            return new Promise((_, reject) => reject(results));
-        }
-
-        // A container is dirty if any of its dirtyable components are dirty:
-        const isDirty = Array.from(parent.inputs())
-            .filter((element) => element.vComponent)
-            .map((element) => element.vComponent)
+        // We're in a dirty state if any dirtyable inputs are dirty:
+        const dirty = this.inputs()
+            .filter((input) => input.vComponent)
+            .map((input) => input.vComponent)
             .filter((comp) => comp.isDirty)
             .map((comp) => comp.isDirty())
             .some(Boolean);
@@ -60,15 +41,15 @@ export class VPromptIfDirty extends VBase {
         results.push({
             action: 'prompt_if_dirty',
             squelch: true,
-            dirty: isDirty,
+            dirty: dirty,
         });
 
-        if (!isDirty) {
+        // If everything's clean, skip the dialog entirely and proceed:
+        if (!dirty) {
             return new Promise((resolve, _) => resolve(results));
         }
 
-        // If the container is dirty, invoke the specified dialog and capture
-        // the result:
+        // Otherwise, invoke the specified dialog and capture its result:
         return new Promise((resolve, reject) => {
             return this.dialog.call(results).then((results) => {
                 const result = results.pop();
@@ -83,5 +64,35 @@ export class VPromptIfDirty extends VBase {
                 return resolve(results);
             });
         });
+    }
+
+    /**
+     * inputs returns an array of Voom components.
+     * If an input_tag has been specified, the array contains input
+     * components tagged with the specified input_tag.
+     * Otherwise, the array contains the parent's input components.
+     * @throws Error if No input_tag is specified and a parent container is
+     *               not present.
+     * @return {array} An array of input components
+     */
+    inputs() {
+        const parent = this.parentElement();
+        const parentComp = parent ? parent.vComponent : null;
+        const inputTag = this.options.input_tag;
+
+        // A specified input_tag has priority over the parent container:
+        if (inputTag) {
+            return this.taggedInputs();
+        }
+
+        // If no parent container can be found, bail:
+        if (!parentComp) {
+            throw new Error(
+                'Unable to find a parent container! Try using an input_tag.'
+            );
+        }
+
+        // Otherwise, use the parent container's input elements:
+        return parentComp.inputs();
     }
 }
