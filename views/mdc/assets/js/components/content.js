4c0c5aca9c48ea7435320c38aa6e2bfd363b07f3
Russell Edens <rx@voomify.com>
2019-02-21 12:06:57 -0500

    Added click away and escape behavior to floating cards.

--- views/mdc/assets/js/components/content.js
+++ views/mdc/assets/js/components/content.js
@@ -1,5 +1,5 @@
-import {VBaseContainer} from "./base-container";
-import {hookupComponents} from "./base-component";
+import {VBaseContainer} from './base-container';
+import {hookupComponents} from './base-component';
 import {eventHandlerMixin} from './mixins/event-handler';
 
 export function initContent() {
@@ -10,5 +10,38 @@ export function initContent() {
 export class VContent extends eventHandlerMixin(VBaseContainer) {
     constructor(element, mdcComponent) {
         super(element, mdcComponent);
+        if (element.dataset.float === 'true') {
+            VContent.hideOnClickOutside(element);
+            VContent.hideOnEscape(element);
+        }
+    }
+
+    static hideOnClickOutside(element) {
+        const outsideClickListener = (event) => {
+            // or use: event.target.closest(selector) === null
+            if (!element.contains(event.target) &&
+                VContent.isVisible(element)) {
+                element.classList.add('v-hidden');
+            }
+        };
+
+        document.addEventListener('click', outsideClickListener);
+    }
+
+    static hideOnEscape(element) {
+        const escapeKeyListener = (event) => {
+            // or use: event.target.closest(selector) === null
+            if (event.which === 27) {
+                element.classList.add('v-hidden');
+            }
+        };
+
+        document.addEventListener('keydown', escapeKeyListener);
+    }
+
+    static isVisible(elem) {
+        // source (2018-03-11): https://github.com/jquery/jquery/blob/master/src/css/hiddenVisibleSelectors.js
+        return !!elem && !!(elem.offsetWidth || elem.offsetHeight ||
+            elem.getClientRects().length);
     }
 }
