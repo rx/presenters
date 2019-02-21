a2c0316b2b933bc1598693e7049ace6b71993d00
Derek Graham <derek@mynorth.com>
2019-02-21 10:47:46 -0500

    Start on attempt to refactor filewatch/reloading

--- lib/voom/presenters-engine.rb
+++ lib/voom/presenters-engine.rb
@@ -1,3 +1,4 @@
+require 'filewatcher'
 if Rails.version =~ /^4/
   ActiveSupport::Dependencies.require_dependency 'voom/presenters'
 else
@@ -18,25 +19,28 @@ unless defined?(Voom::Presenter::Railtie)
     module Presenter
       class Railtie < ::Rails::Railtie
         BOOT = -> {
+          Voom::Presenters::App.reset!
           Voom::Presenters::Settings.configure do |config|
             config.presenters.root = Rails.root.join('app')
             config.presenters.deep_freeze = false
           end
           Voom::Presenters::App.boot!
         }
-        RELOADER = ActiveSupport::FileUpdateChecker.new([], {"app" => ["pom"]}) do
-          Voom::Presenters::App.reset!
+
+        WATCH = -> {
+          filewatcher = Filewatcher.new(Rails.root.join('app', '**', '*.pom'))
+          Thread.new(filewatcher) do |fw|
+            fw.watch do |f|
+              puts "Updated file: #{f}"
+              BOOT.call
+            end
+          end
+        }
+
+        config.after_initialize do
           BOOT.call
-        end
-
-        initializer 'voom-presenters' do |app|
-          app.reloaders << RELOADER
-        end
-
-        config.to_prepare do
-          reloaded = RELOADER.execute_if_updated
-          BOOT.call unless reloaded
           require_dependency Voom::Presenter::Engine.root.join('lib', 'voom-presenters').to_s
+          WATCH.call # if DEV
         end
       end
     end
