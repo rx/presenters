0c9aca59b0e3f99ca08eb1631200ddc2afc04581
jadefish <nick@nmiller.info>
2019-02-26 14:23:36 -0500

    Move on_value and off_value accessors to toggle_base

--- lib/voom/presenters/dsl/components/switch.rb
+++ lib/voom/presenters/dsl/components/switch.rb
@@ -5,11 +5,8 @@ module Voom
     module DSL
       module Components
         class Switch < ToggleBase
-          attr_accessor :on_value, :off_value
           def initialize(**attribs_, &block)
             super(type: :switch, **attribs_, &block)
-            @on_value = attribs.delete(:on_value){ 'on' }
-            @off_value = attribs.delete(:off_value){ nil }
             expand!
           end
         end
--- lib/voom/presenters/dsl/components/toggle_base.rb
+++ lib/voom/presenters/dsl/components/toggle_base.rb
@@ -7,7 +7,13 @@ module Voom
       module Components
         # Base class used by toggle classes
         class ToggleBase < Input
-          attr_accessor :text, :text_color, :checked, :value, :disabled
+          attr_accessor :text,
+                        :text_color,
+                        :checked,
+                        :value,
+                        :disabled,
+                        :on_value,
+                        :off_value
          
           def initialize(**attribs_, &block)
             super(**attribs_, &block)
@@ -16,6 +22,8 @@ module Voom
             @checked = attribs.delete(:checked) { false }
             @value = attribs.delete(:value)
             @disabled = attribs.delete(:disabled) { false }
+            @on_value = attribs.delete(:on_value) { 'on' }
+            @off_value = attribs.delete(:off_value)
           end
         end
       end
