d4e8cba79e01a91415ccb5c285eef474edc2cc7a
Kurt Grafius <302367+kgrafius@users.noreply.github.com>
2019-04-19 19:07:32 -0700

    Added height attribute to content componenet

--- lib/voom/presenters/dsl/components/content.rb
+++ lib/voom/presenters/dsl/components/content.rb
@@ -35,7 +35,7 @@ module Voom
           include Mixins::FileInputs
           include Mixins::Avatar
 
-          attr_reader :hidden, :float, :components, :shows_errors, :width, :position, :text_align, :padding, :dense
+          attr_reader :hidden, :float, :components, :shows_errors, :width, :height,  :position, :text_align, :padding, :dense
 
           def initialize(**attribs_, &block)
             super(type: :content, **attribs_, &block)
@@ -43,6 +43,7 @@ module Voom
             @hidden = attribs.delete(:hidden){false}
             @float = attribs.delete(:float){false}
             @width = attribs.delete(:width){nil}
+            @height = attribs.delete(:height){nil}
             @shows_errors = attribs.delete(:shows_errors){false}
             @position = Array(attribs.delete(:position)).compact
             @text_align = attribs.delete(:text_align){'left'}
