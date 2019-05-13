d540a1ec4a6c50d06eb169dfcfb75613a5021c83
Russell Edens <rx@voomify.com>
2019-05-13 10:12:13 -0400

    Fixed circular dependency.

--- lib/voom/presenters/dsl/components/mixins/common.rb
+++ lib/voom/presenters/dsl/components/mixins/common.rb
@@ -9,7 +9,6 @@ require 'voom/presenters/dsl/components/mixins/menus'
 require 'voom/presenters/dsl/components/mixins/google_maps'
 require 'voom/presenters/dsl/components/mixins/tab_bars'
 require 'voom/presenters/dsl/components/mixins/images'
-require 'voom/presenters/dsl/components/mixins/progress'
 require 'voom/presenters/dsl/components/mixins/dialogs'
 
 module Voom
@@ -28,7 +27,6 @@ module Voom
             include Mixins::Menus
             include Mixins::TabBars
             include Mixins::Images
-            include Mixins::Progress
             include Mixins::Dialogs
 
             def badge(badge=nil, **attributes, &block)
