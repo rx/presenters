d0c83e20c437b51e2612fb6619c2ff542ed3be46
jadefish <nick@nmiller.info>
2019-06-01 18:07:41 -0400

    Ensure expandParams preserve simple values
    This change ensures `expandParams` does not eliminate
    non-`action_parameter` values when resolving objects with both types of
    parameters.

--- views/mdc/assets/js/components/events/action_parameter.js
+++ views/mdc/assets/js/components/events/action_parameter.js
@@ -16,24 +16,41 @@ class VActionParameter {
     }
 }
 
-export function expandParam(results, value) {
-    if (value.type && value.type === 'action_parameter') {
-        return new VActionParameter(value).fetchValue(results);
-    }
-    else {
-        return value;
-    }
+function isObject(thing) {
+    return thing && typeof thing === 'object';
 }
 
+/**
+ * expandParam resolves an parameter `value` to a primitive value
+ * according to the given path for the parameter in `results`.
+ * If the `value` is not an action_parameter, it is returned unaltered.
+ * @param {Object} results An action's results
+ * @param {*} value The value of the parameter
+ * @return {*} A resolved primitive value
+ */
+export function expandParam(results, value) {
+    if (isObject(value) && value.type === 'action_parameter') {
+        return new VActionParameter(value).fetchValue(results);
+    }
+
+    return value;
+}
+
+/**
+ * expandParams resolves all values in `params` to primitive values.
+ *
+ * Primitive values are passed through unaltered.
+ * Values of action_parameter parameters are resolved to primitive values
+ * via `results`.
+ * @param {Object} results An action's results
+ * @param {Object} params An action's parameters
+ * @return {Object}
+ */
 export function expandParams(results, params) {
     const expandedParams = {};
 
     for (const [key, value] of Object.entries(params)) {
-        if (typeof value !== 'object') {
-            continue;
-        }
-
-        if (value.type === 'action_parameter') {
+        if (!isObject(value) || value.type === 'action_parameter') {
             expandedParams[key] = expandParam(results, value);
         }
         else {
