5dd5a1a028a4f6110addae3f0ed528585d59b05f
Kurt Grafius <302367+kgrafius@users.noreply.github.com>
2019-07-01 17:37:46 -0700

    Some minro tweaks to the Drag And Drop styling

--- public/style-bundle.js
+++ public/style-bundle.js
@@ -65,9 +65,9 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-throw new Error("Module build failed: Error: Node Sass does not yet support your current environment: OS X 64-bit with Unsupported runtime (72)\nFor more information on which environments are supported please see:\nhttps://github.com/sass/node-sass/releases/tag/v4.11.0\n    at module.exports (/Users/kurt/dev/rx/presenters/views/mdc/node_modules/node-sass/lib/binding.js:13:13)\n    at Object.<anonymous> (/Users/kurt/dev/rx/presenters/views/mdc/node_modules/node-sass/lib/index.js:14:35)\n    at Module._compile (internal/modules/cjs/loader.js:759:30)\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:770:10)\n    at Module.load (internal/modules/cjs/loader.js:628:32)\n    at Function.Module._load (internal/modules/cjs/loader.js:555:12)\n    at Module.require (internal/modules/cjs/loader.js:666:19)\n    at require (internal/modules/cjs/helpers.js:16:16)\n    at Object.sassLoader (/Users/kurt/dev/rx/presenters/views/mdc/node_modules/sass-loader/lib/loader.js:46:72)");
+module.exports = __webpack_require__.p + "../../public/bundle.css";
 
 /***/ })
 /******/ ]);
