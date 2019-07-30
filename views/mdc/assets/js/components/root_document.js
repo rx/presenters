08fd9df9c837bc9089c929ec700ec79ae50b4fdf
Russell Edens <rx@voomify.com>
2019-07-30 12:39:55 -0400

    Rolled back webcomponents change

--- views/mdc/assets/js/components/root_document.js
+++ views/mdc/assets/js/components/root_document.js
@@ -1,18 +1,8 @@
 export default function getRoot(element) {
-    let e = element;
-    while (e && e.nodeType != 11) { // 11 = DOCUMENT_FRAGMENT_NODE
-        e = e.parentNode;
+    if (element instanceof HTMLDocument) return element;
+    let rootElement = element.ownerDocument;
+    if (!rootElement) {
+        rootElement = element.shadowRoot();
     }
-    let shadowRoot = null;
-    if (e) {
-        shadowRoot = e.host.shadowRoot;
-    }
-
-    let documentRoot = null;
-    if (element.nodeType == 9) {
-        documentRoot = element;
-    }
-    const root = shadowRoot || documentRoot || element.ownerDocument;
-    root.document = documentRoot || element.ownerDocument;
-    return root;
+    return rootElement;
 }
