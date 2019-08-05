6c07a5bf2a18752e5304a5f0356dc6e806ce7a3d
Russell Edens <rx@voomify.com>
2019-08-05 13:01:38 -0400

    Made padding backward compatible.

--- lib/voom/presenters/web_client/helpers/padding.rb
+++ lib/voom/presenters/web_client/helpers/padding.rb
@@ -2,7 +2,7 @@
 module PaddingHelpers
   def _padding_array_(padding, nesting=0)
     return (%i(top0 right0 bottom0 left0) - padding.map {|p| "#{p.to_s.gsub(%r(\d),'')}0".to_sym} + padding.map(&:to_sym)).sort if padding
-    nesting > 1 ? %i(top3 right0 bottom3 left0).sort : %i(top0 right0 bottom0 left0).sort
+    nesting > 1 ? %i(top3 right0 bottom3 left0).sort : []
   end
 
   def _padding?(padding, nesting=0)
