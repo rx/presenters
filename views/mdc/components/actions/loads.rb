f0f12eae024af23f9e78b6fd4b9e53216a173072
Russell Edens <rx@voomify.com>
2019-08-25 11:39:36 -0400

    Fixed spec - making options a hash before using.

--- views/mdc/components/actions/loads.rb
+++ views/mdc/components/actions/loads.rb
@@ -6,9 +6,10 @@ module WebClient
       include ExpandHash
       def call(action, *)
         # Type, URL, Options, Params (passed into javascript event/action classes)
+        safe_options = action.options.to_h
         [action.type, action.url,
-         action.options.to_h.merge({input_tag: action.options.fetch(:input_tag, nil),
-                                    wait_for_download: action.options.fetch(:wait_for_download, false)}),
+         safe_options.merge({input_tag: safe_options.fetch(:input_tag, nil),
+                                    wait_for_download: safe_options.fetch(:wait_for_download, false)}),
          expand_hash(action.dynamic_params)]
       end
     end
