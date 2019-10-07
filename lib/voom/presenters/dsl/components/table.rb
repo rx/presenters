c8c923aaea066d7ea5c0a6f97ffedb697851ffff
Kurt Grafius <302367+kgrafius@users.noreply.github.com>
2019-10-07 13:06:07 -0700

    Added width support to table component

--- lib/voom/presenters/dsl/components/table.rb
+++ lib/voom/presenters/dsl/components/table.rb
@@ -12,11 +12,12 @@ module Voom
         class Table < Base
           include Mixins::Common
           include Mixins::Event
-          attr_accessor :header, :rows, :selectable
+          attr_accessor :header, :rows, :selectable, :width
 
           def initialize(**attribs_, &block)
             super(type: :table, **attribs_, &block)
             @selectable = attribs.delete(:selectable)
+            @width = attribs.delete(:width)
             @rows = []
             expand!
           end
