8f41dd3cd1a014add9b39571746d376f01d29093
kgrafius <kgrafius@gmail.com>
2019-12-06 12:34:08 -0800

    Added new `number_field` component that supports the input type='number' to show a text filed with up and down arrows for scrolling through a set of numbers. Also DRY'd up some of the markup related to input labels and help/hint text.

--- lib/voom/presenters/dsl/components/number_field.rb
+++ lib/voom/presenters/dsl/components/number_field.rb
@@ -3,13 +3,14 @@ module Voom
     module DSL
       module Components
         class NumberField < TextField
-          attr_reader :min, :max, :step
+          attr_reader :min, :max, :step, :readonly
 
           def initialize(**attribs_, &block)
             super(type: :number_field, **attribs_, &block)
             @min = attribs.delete(:min)
             @max = attribs.delete(:max)
             @step = attribs.delete(:step)
+            @readonly = attribs.delete(:readonly){false}
             expand!
           end
         end
