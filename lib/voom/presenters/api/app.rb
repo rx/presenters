52f2bb98b18a177fb2b02201ebee19552fbedd20
jadefish <nick@nmiller.info>
2020-01-22 16:56:45 -0500

    Add before_render configuration setting
    The `before_render` configuration setting holds an array of objects
    which respond to `#call/1`. Each is lazily invoked with the current
    request before a Presenter is rendered (in both the web client and the
    API app).
    
    If a `before_render` item returns a non-`nil` registered Presenter name,
    the Presenter identified by the returned name will be rendered instead
    of the original Presenter.
    
    If all `before_render` items return `nil`, rendering is not intercepted,
    and the original Presenter is rendered unhindered.
    
    `before_render` items can also optionally return a context hash which
    will be merged with the request's original context. The returned context
    hash from `before_render` will destructively overwrite keys on the
    request's original context hash.
    
    This setting and its behaviors are documented in the new documentation
    file `docs/settings.md`.

--- lib/voom/presenters/api/app.rb
+++ lib/voom/presenters/api/app.rb
@@ -33,16 +33,28 @@ module Voom
         # analogous to Voom::Presenters::WebClient::App#render_presenter
         def render_presenter(presenter)
           begin
+            before_render = Presenters::Settings.config.presenters.before_render
+            render_instead, ctx = before_render
+                                    .lazy
+                                    .map { |p| p.call(request) }
+                                    .detect(&:itself)
+
+            if Presenters::App.registered?(render_instead)
+              presenter = render_instead
+            end
+
+            p = params.merge(ctx || {})
             presenter = Voom::Presenters::App[presenter].call
-            pom = presenter.expand(router: router, context: prepare_context)
+            pom = presenter.expand(router: router, context: prepare_context(p))
             content_type :json
             JSON.dump(pom.to_hash)
           rescue StandardError => e
+            presenter_name = presenter.respond_to?(:name) ? presenter.name : '(unknown)'
             Presenters::Settings.config.presenters.error_logger.call(
               @env['rack.errors'],
               e,
               params,
-              presenter.name
+              presenter_name
             )
             raise e
           end
@@ -52,10 +64,10 @@ module Voom
           settings.router_.new(base_url: "#{request.base_url}#{env['SCRIPT_NAME']}")
         end
 
-        def prepare_context
+        def prepare_context(base_params = params)
           prepare_context = Presenters::Settings.config.presenters.web_client.prepare_context.dup
           prepare_context.push(method(:scrub_context))
-          context = params.dup
+          context = base_params.dup
           prepare_context.reduce(context) do |params, context_proc|
             context_proc.call(params, session, env)
           end
--- lib/voom/presenters/settings.rb
+++ lib/voom/presenters/settings.rb
@@ -68,6 +68,7 @@ unless defined?(Voom::Presenters::Settings)
             ].join("\n\t")
             file.puts(msg)
           }
+          setting :before_render, [] # an array of `#call`ables
         end
 
         def self.default(type, key)
