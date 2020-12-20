a944028b05081011600fde7ab574efae9d5e29be
rx <rx@voomify.com>
2020-12-20 14:47:07 -0500

    chore: removed old release process in favor of action ci driven semantic releases.
    BREAKING CHANGE: trigering major release

--- scripts/changelog.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-# https://github.com/github-changelog-generator/github-changelog-generator
-github_changelog_generator rx/presenters --future-release $1
-
--- scripts/release.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-# Docs: https://github.com/svenfuchs/gem-release#gem-release
-gem release
-
