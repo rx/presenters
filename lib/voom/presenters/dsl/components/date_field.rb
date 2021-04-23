bae87cf887b9cea71b624a20ea256af4657e4c88
Derek Graham <derek@evvnt.com>
2021-04-22 21:55:15 -0400

    Add an option to date_field component to use it without the picker. This is a temporary measure until wew can update the date/time component to be in line with the latest Material Design [date picker](https://material.io/components/date-pickers) spec which provides the user the option of using a picker or not.

--- lib/voom/presenters/dsl/components/date_field.rb
+++ lib/voom/presenters/dsl/components/date_field.rb
@@ -5,14 +5,17 @@ module Voom
     module DSL
       module Components
         class DateField < DatetimeBase
-          attr_reader :config
+          attr_reader :config, :picker
 
           def initialize(**attribs_, &block)
             super(type: :date, **attribs_, &block)
             merge_config(:min_date)
             merge_config(:max_date)
+            @picker = attribs_.delete(:picker){ true }
+            @hint ||= 'Enter date as MM/DD/YYYY' unless @picker
             expand!
           end
+
         end
       end
     end
