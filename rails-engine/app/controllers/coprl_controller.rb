bafa36d6e95ad9863eb93cb44cc894163c147c4f
Rx <russell@russelledens.net>
2021-06-07 16:06:04 -0400

    fix: added missing prepare context callbacks to Rails Engine

--- rails-engine/app/controllers/coprl_controller.rb
+++ rails-engine/app/controllers/coprl_controller.rb
@@ -12,7 +12,7 @@ class CoprlController < ApplicationController
     fq_presenter_name = [presenter_name, 'index'].compact.join(':')
     presenter_name = fq_presenter_name if Coprl::Presenters::App.registered?(fq_presenter_name)
     presenter = Coprl::Presenters::App[presenter_name].call
-    context = params.dup.to_unsafe_hash
+    context = prepare_context(params)
     router = Coprl::Presenters::WebClient::Router.new(base_url: request.base_url)
     @pom = presenter.expand(router: router, context: context)
   end
@@ -22,5 +22,22 @@ class CoprlController < ApplicationController
     paths.each do |path|
       prepend_view_path path
     end
+  end
+
+  def prepare_context(base_params = params)
+    prepare_context = Coprl::Presenters::Settings.config.presenters.web_client.prepare_context.dup
+    prepare_context.push(method(:scrub_context))
+    context = base_params.dup.to_unsafe_hash
+    prepare_context.reduce(context) do |params, context_proc|
+      context = context_proc.call(params, session, request.env)
+    end
+    context
+  end
+
+  def scrub_context(params, _session, _env)
+    %i(grid_nesting input_tag).each do |key|
+      params.delete(key) {params.delete(key.to_s)}
+    end
+    params
   end
 end
