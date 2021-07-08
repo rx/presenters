2a917e7076895037e5a069e77525ef53d2ca45f1
rhetprieto <40408815+rhetprieto@users.noreply.github.com>
2021-07-08 15:11:54 -0600

    fix: view paths issue when using partials with Rails Engine (#321)
    * Use concern to prepare view paths
    * Fix Rails 5 bug when adding Rails URL helpers
    * fix unnecessary space

--- lib/coprl/presenters/web_client/plugin_views_path.rb
+++ lib/coprl/presenters/web_client/plugin_views_path.rb
@@ -8,17 +8,17 @@ module Coprl
         extend Pluggable
         include_plugins(:WebClientComponents)
 
-        def initialize(pom:)
+        def initialize(pom: nil, plugins: nil)
           @pom = pom
+          @plugins = plugins || []
           initialize_plugins
         end
 
         def render
           results = []
-          ((@plugins||[]) + Coprl::Presenters::Settings.config.presenters.plugins).each do |plugin|
+          (@plugins + Coprl::Presenters::Settings.config.presenters.plugins).each do |plugin|
             view_dir_method = :"view_dir_#{plugin}"
-            results << send(view_dir_method,
-                 @pom) if respond_to?(view_dir_method)
+            results << send(view_dir_method, @pom) if respond_to?(view_dir_method)
           end
           results
         end
@@ -26,7 +26,7 @@ module Coprl
         private
 
         def initialize_plugins
-          @plugins = @pom.send(:plugins)
+          @plugins << @pom.send(:plugins) if @pom
           self.class.include_plugins(:WebClientComponents, plugins: @plugins)
         end
       end
