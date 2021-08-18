2f3058ea844d0f1e04b9c7d88df02a2f4e17794d
Derek Graham <derek@evvnt.com>
2021-08-18 14:09:27 -0400

    Update browser detection - Facebook iOS in-app browser is not detectable, allow app to proceed when browser name is 'unknown'.

--- views/mdc/assets/js/utils/compatibility.js
+++ views/mdc/assets/js/utils/compatibility.js
@@ -4,6 +4,6 @@ import config from '../config';
 const incompatibleList = config.get('compatibility.incompatibleBrowsers', []);
 const browser = detect();
 const name = browser ? browser.name : 'unknown';
-const isCompatible = browser && incompatibleList.indexOf(name) < 0;
+const isCompatible = name && incompatibleList.indexOf(name) < 0;
 
 export default isCompatible;
