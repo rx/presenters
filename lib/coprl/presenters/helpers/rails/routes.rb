aa2d0ce72373e5ed58dac2fea8ff9790a4384d49
Russell Edens <russell@voomify.com>
2021-09-21 10:22:33 -0400

    fix: added support for api base base_url
    When running the API as rack app mounted inside a rails app the traditional means of finding the base url do not work.  The Api sinatra app is adding a base_url to the context. This change honors that when it's present.

--- lib/coprl/presenters/helpers/rails/routes.rb
+++ lib/coprl/presenters/helpers/rails/routes.rb
@@ -5,7 +5,7 @@ module Coprl
         module Routes
           include ::Rails.application.routes.url_helpers
           def default_url_options
-            ::Rails.application.config.action_controller.default_url_options || { :host => context[:request].host_with_port }
+            ::Rails.application.config.action_controller.default_url_options || { :host => context[:base_url] ? context[:base_url] : context[:request].host_with_port }
           end
         end
       end
